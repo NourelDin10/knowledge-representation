e8d3fb7ae87c9386bfb6357cdc1abc20138912d4
NourelDin10 <92956059+NourelDin10@users.noreply.github.com>
2021-12-08 00:00:45 +0200

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -8,9 +8,6 @@ orange = Symbol("orange")
 knowledge_for_question1 = And(
     Implication(carrots, orange)
 )
-# print(knowledge_for_question1.formula())  # print the output formula of knowledge base
-#---------------------------------------------------
-
 
 # question 1.2
 # in English : " if person is vegetarian , person likes carrots "
@@ -19,23 +16,19 @@ vegetarian = Symbol("vegetarian(x)")
 like = Symbol("like")
 like_person_carrots = Symbol("like(x, carrots)")
 knowledge_for_question2 = And(Implication(vegetarian, like_person_carrots))
-# print(knowledge_for_question2.formula())  # print the output formula of knowledge base
-#---------------------------------------------------
+
 
 # question 1.3
 # in English : " student pass if he studies hard "
 pass_exam = Symbol("pass(x)")
 study_hard = Symbol("study_hard(x)")
 knowledge_for_question3 = Implication(study_hard, pass_exam)
-# print(knowledge_for_question3.formula())  # print the output formula of knowledge base
-#---------------------------------------------------
 
 # question 1.4
 # in English : " who will pass? "
 Pass = Symbol("? pass(who)")
 knowledge_for_question4 = And(Pass)
-# print(knowledge_for_question4.formula())  # print the output formula of knowledge base
-#---------------------------------------------------
+
 
 # question 1.5
 # in English : " which course teacher teach ? "
@@ -51,4 +44,3 @@ enemies = Symbol(f"enemies({x}, {y})")
 hates = Symbol(f"hates({x}, {y})")
 fight = Symbol(f"fight({x}, {y})")
 knowledge_for_question6 = And(Implication(enemies, And(hates, fight)))
-# print(knowledge_for_question6.formula()) # print the output formula of knowledge base
